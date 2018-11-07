ee64fc95cb84875ccf55e99d47c63180543fee03
luca <l.radaelli85@noreply.github.com>
2018-11-07 12:32:07 +0100

    [IMP] improved readme and add configuration parameters

--- QoS.sh
+++ QoS.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
+
 #load configurations
 . qos.cfg
 
-function system_stuff(){
-
+function add_system_configurations(){
 #https://bugzilla.redhat.com/show_bug.cgi?id=1011281
 #echo 1 >/sys/module/sch_htb/parameters/htb_rate_est
 
 #load ifb module
 modprobe ifb
+
 #load act_mirred module
 modprobe act_mirred
+
 #set ifb device as UP
 ip link set dev $IFB up
-#Outgoing NAT
-iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
+
 #Enable ip forwarding,otherwise device behind this GW won't reach Internet
 sysctl -w net.ipv4.ip_forward=1
-
 }
 
-function iptables_rules(){
-#Qos chains
-iptables -t mangle -N QOS_UPLOAD
-iptables -t mangle -N QOS_DOWNLOAD
-iptables -t mangle -N QOS_SLOWDOWN
-iptables -t mangle -N RESTORE-MARK
-iptables -t mangle -N SAVE-MARK
-iptables -t mangle -N QOS_DPI
-iptables -t mangle -A FORWARD -j QOS_SLOWDOWN
-iptables -t mangle -A FORWARD -m mark --mark 0 -o $WAN -j QOS_DOWNLOAD
-iptables -t mangle -A FORWARD -o $WAN -m mark ! --mark 0 -j QOS_UPLOAD
-iptables -t mangle -A PREROUTING -m connmark ! --mark 0 -j RESTORE-MARK
-iptables -t mangle -A POSTROUTING -m mark ! --mark 0 -j SAVE-MARK
+function del_system_configurations(){
+#disable forwarding
+sysctl -w net.ipv4.ip_forward=0
 
-#restore mark for previously marked connection
-iptables -t mangle -A RESTORE-MARK -m conntrack ! --ctstate NEW -j CONNMARK --restore-mark
+#remove Qos modules
+rmmod ifb
+rmmod act_mirred
+}
+
+function l7_classification(){
+iptables -t mangle -N QOS_DPI
 
 #Send everything to nDPI to applications detection
 iptables -t mangle -A PREROUTING -j QOS_DPI
@@ -45,13 +40,33 @@ iptables -t mangle -A QOS_DPI -m connmark --mark $APP_LOW_PRIO_MARK -j RETURN
 iptables -t mangle -A QOS_DPI -m connmark --mark $APP_BULK_MARK -j RETURN
 iptables -t mangle -A QOS_DPI -m connmark --mark $APP_HIGH_PRIO_MARK -j RETURN
 iptables -t mangle -A QOS_DPI -m connbytes --connbytes-mode packets --connbytes-dir both --connbytes 20 -j RETURN
-iptables -t mangle -A QOS_DPI -m ndpi --youtube -j CONNMARK --set-mark $APP_LOW_PRIO_MARK
-iptables -t mangle -A QOS_DPI -m ndpi --facebook -j CONNMARK --set-mark $APP_BULK_MARK
-iptables -t mangle -A QOS_DPI -m ndpi --dropbox -j CONNMARK --set-mark $APP_HIGH_PRIO_MARK
+if [ ${#LOW_PRIO_APP[@]} -gt 0 ]
+  then
+    for app in ${LOW_PRIO_APP[@]}
+    do
+      iptables -t mangle -A QOS_DPI -m ndpi --$app -j CONNMARK --set-mark $APP_LOW_PRIO_MARK
+    done
+fi
+if [ ${#BULK_PRIO_APP[@]} -gt 0 ]
+  then
+    for app in ${BULK_PRIO_APP[@]}
+    do
+      iptables -t mangle -A QOS_DPI -m ndpi --$app -j CONNMARK --set-mark $APP_BULK_MARK
+    done
+fi
+if [ ${#HIGH_PRIO_APP[@]} -gt 0 ]
+  then
+    for app in ${HIGH_PRIO_APP[@]}
+    do
+      iptables -t mangle -A QOS_DPI -m ndpi --$app -j CONNMARK --set-mark $APP_HIGH_PRIO_MARK
+    done
+fi
+}
 
-if [ $ENABLE_SLOWDOWN == "on" ]
-then
-#slow down if traffic generated is higher than 30MB
+function slowdown(){
+iptables -t mangle -N QOS_SLOWDOWN
+iptables -t mangle -I FORWARD -j QOS_SLOWDOWN
+
 iptables -t mangle -A QOS_SLOWDOWN \
 -p tcp -m multiport --dports 80,443,20,21,990 -m connbytes --connbytes $SLOWDOWN_QUOTA: \
 --connbytes-dir both --connbytes-mode bytes -j CONNMARK --set-mark $DOWN_LOW_PRIO_MARK
@@ -59,21 +74,66 @@ iptables -t mangle -A QOS_SLOWDOWN \
 iptables -t mangle -A QOS_SLOWDOWN \
 -p tcp -m multiport --dports 80,443,20,21,990 -m connbytes --connbytes $SLOWDOWN_QUOTA: \
 --connbytes-dir both --connbytes-mode bytes -j RETURN
+}
+
+function add_iptables_rules(){
+#Outgoing NAT
+iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
+#Qos chains
+iptables -t mangle -N QOS_UPLOAD
+iptables -t mangle -N QOS_DOWNLOAD
+iptables -t mangle -N RESTORE-MARK
+iptables -t mangle -N SAVE-MARK
+iptables -t mangle -A FORWARD -m mark --mark 0 -o $WAN -j QOS_DOWNLOAD
+iptables -t mangle -A FORWARD -o $WAN -m mark ! --mark 0 -j QOS_UPLOAD
+iptables -t mangle -A PREROUTING -m connmark ! --mark 0 -j RESTORE-MARK
+iptables -t mangle -A POSTROUTING -m mark ! --mark 0 -j SAVE-MARK
+
+#restore mark for previously marked connection
+iptables -t mangle -A RESTORE-MARK -m conntrack ! --ctstate NEW -j CONNMARK --restore-mark
+
+if [ $ENABLE_L7 == "on" ]
+  then
+    l7_classification
+fi
+
+if [ $ENABLE_SLOWDOWN == "on" ]
+  then
+    slowdown
 fi
 
 #high prio traffic
+#http(s),ssh
 iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports 80,443,22 \
 -m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_HIGH_PRIO_MARK
 
 iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports 80,443,22 \
 -m conntrack --ctstate NEW -j RETURN
 
+#voip,dns,ipsec,openvpn
+iptables -t mangle -A QOS_DOWNLOAD -p udp -m multiport --dports 5060,53,4500,500,1194 \
+-m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_HIGH_PRIO_MARK
+
+iptables -t mangle -A QOS_DOWNLOAD -p udp -m multiport --dports 5060,53,4500,500,1194 \
+-m conntrack --ctstate NEW -j RETURN
+
+for prot in tcp udp
+do
+#bulk traffic
+iptables -t mangle -A QOS_DOWNLOAD -p $prot -m multiport --dports 0:1023 \
+-m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_BULK_MARK
+
+iptables -t mangle -A QOS_DOWNLOAD -p $prot -m multiport --dports 0:1023 \
+-m conntrack --ctstate NEW -j RETURN
+
 #low prio traffic
-iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports 1024:65535 \
+iptables -t mangle -A QOS_DOWNLOAD -p $prot -m multiport --dports 1024:65535 \
 -m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_LOW_PRIO_MARK
 
-iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports 1024:65535 \
+iptables -t mangle -A QOS_DOWNLOAD -p $prot -m multiport --dports 1024:65535 \
 -m conntrack --ctstate NEW -j RETURN
+
+done
 
 #In this way i should not need to mark connection for upload,and the mark for download traffic shuold be maintained
 #high prio traffic
@@ -88,12 +148,10 @@ iptables -t mangle -A QOS_UPLOAD -m mark --mark $DOWN_BULK_MARK -j RETURN
 iptables -t mangle -A SAVE-MARK -m conntrack --ctstate NEW -j CONNMARK --save-mark
 }
 
-function start(){
-
-system_stuff
+function add_qos_devs_and_classes(){
 #######################DOWNLOAD#############################################
 #Defult class is bulk traffic class
-tc qdisc add dev $IFB root handle 1: htb default $DOWN_BULK_MARK
+tc qdisc add dev $IFB root handle 1: htb #default $DOWN_BULK_MARK
 
 #set global download value
 tc class add dev $IFB parent 1: classid 1:1 htb rate $WAN_DOWNLOAD burst 15k
@@ -132,9 +190,9 @@ tc qdisc add dev $WAN handle ffff: ingress
 tc filter add dev $WAN parent ffff: protocol ip u32 match u32 0 0 action \
 connmark action mirred egress redirect dev $IFB
 
-# ######################UPLOAD################################################
+#######################UPLOAD################################################
 #default class is bulk traffic class
-tc qdisc add dev $WAN root handle 1:0 htb default $UP_BULK_MARK
+tc qdisc add dev $WAN root handle 1:0 htb #default $UP_BULK_MARK
 
 #set the global upload value
 tc class add dev $WAN parent 1: classid 1:1 htb rate $WAN_UPLOAD burst 15k
@@ -148,20 +206,20 @@ tc class add dev $WAN parent 1:1 classid 1:$UP_LOW_PRIO_MARK htb rate $LOW_PRIO_
 #bulk traffic class
 tc class add dev $WAN parent 1:1 classid 1:$UP_BULK_MARK htb rate $UPLOAD_GUARANTEED_DEFAULT ceil $UPLOAD_MAX_DEFAULT quantum 1514 burst 15k prio 5
 
-# Tell which algorithm the classes use
+#Tell which algorithm the classes use
 tc qdisc add dev $WAN parent 1:$UP_HIGH_PRIO_MARK sfq perturb 10
 tc qdisc add dev $WAN parent 1:$UP_LOW_PRIO_MARK sfq perturb 10
 tc qdisc add dev $WAN parent 1:$UP_BULK_MARK sfq perturb 10
-
-iptables_rules
 }
 
-function stop(){
+function del_qos_dev_and_classes(){
 #remove tc devices
 tc qdisc del dev $WAN ingress
 tc qdisc del dev $WAN root
 tc qdisc del dev $IFB root
+}
 
+function del_iptables_rules(){
 #flush iptables rules
 iptables -t mangle -F SAVE-MARK
 iptables -t mangle -D POSTROUTING -m mark ! --mark 0 -j SAVE-MARK
@@ -182,23 +240,27 @@ iptables -t mangle -F QOS_DPI
 iptables -t mangle -D PREROUTING -j QOS_DPI
 iptables -t mangle -X QOS_DPI
 iptables -t nat -D POSTROUTING -o $WAN -j MASQUERADE
-
-#disable forwarding
-sysctl -w net.ipv4.ip_forward=0
-
-#remove Qos modules
-rmmod ifb
-rmmod act_mirred
 }
 
-function show() {
-  local qos_dev
-  clear
-  echo "## Download QoS classes ##"
-  tc -g -s -nm -cf qos_class_mapping.cfg class show dev $IFB
-  echo " "
-  echo "## Upload QoS classes ##"
-  tc -g -s -nm -cf qos_class_mapping.cfg class show dev $WAN
+function start(){
+add_system_configurations
+add_qos_devs_and_classes
+add_iptables_rules
+}
+
+function stop(){
+del_qos_dev_and_classes
+del_iptables_rules
+del_system_configurations
+}
+
+function show(){
+clear
+echo "## Download QoS classes ##"
+tc -g -s -nm -cf qos_class_mapping.cfg class show dev $IFB
+echo " "
+echo "## Upload QoS classes ##"
+tc -g -s -nm -cf qos_class_mapping.cfg class show dev $WAN
 }
 
 case $1 in
--- README.md
+++ README.md
@@ -1,27 +1,58 @@
 # tc-QOS
 
-Linux Qos built with tc and HTB.
+Linux QoS built with tc and HTB.
 
-In order to run correctly this script you need the latest kernel and iproute2 package.
+#Rquirements
 
-Support for act_mirred and act_connmark is required.
+- iproute2
+- act_mirred and act_connmark kernel support
+- {optional} [nDPI](https://github.com/ntop/nDPI) for L7 classification
 
-This script has been tested and built on a standard debian 9.
+#Features
 
-If you want to be able to detect applications you need to compile nDPI.
-https://github.com/ntop/nDPI
+- Handle Download traffic (i.e traffic coming from WAN)
+- Handle Upload traffic (i.e traffic to WAN)
+- {optional} Classify L7 traffic
+- {optional} Slowdown (set a low priority) those connections that go beyond a fixed amount of traffic (i.e big download)
 
-On each class there are values that define the guaranteed bandwith , the bandwith limit (ceil) and the priority.
+#Introduction
 
-The logic is that Bulk should be used for traffic that does not need to be classified.
+The aim of this script is to guarantee (bandwidth can't go below this value) , limit (maximum bandwidth usable) and prioritize certain kind of traffic.
 
-This script set as bulk traffic all the traffic that is not classified as High/Low priority.
+It has been designed to share the same internet link between different sources.
 
-High/Low priority classes are self-explained.
+This is done by using classes.
+
+*download/upload/guaranteed/limit/applications values present in qos.cfg have been used only as reference*
+
+This script has been designed to use three different kind of classes:
+
+#### - Bulk traffic
+
+This class has a low guaranteed bandwidth,medium priority and it can use all the available up/down bandwidth
+All low-ports (0-1023, except some) are classified as bulk traffic.
+Modify the QoS.sh file if this is not what you want
+
+#### - Low priority traffic
+
+Traffic that has to be limited (small amount of available bandwidth) with a low priority.
+All high-ports (1024-65535, except some) are classified as low priority traffic.
+YOUTUBE,TWITTER,FACEBOOK,DROPBOX,SPOTIFY applications if L7 is enabled.
+Modify the QoS.sh and qos.cfg files if this is not what you want
+
+#### - High priority traffic
+
+Traffic that has to be prioritized.
+This class has an high guaranteed bandwidth, high priority and it can use all the available up/down bandwidth
+HTTP(s),SSH,DNS,VOIP,IPsec,OpenVPN are classified as high priority traffic.
+STUN,RTP,H323,HANGOUT,SKYPE,OFFICE 365 applications if L7 is enabled.
+Modify the QoS.sh and qos.cfg files if this is not what you want
 
 When a class requests less than the amount assigned, the remaining (excess) bandwidth is distributed to other classes which request service.
 
-Classes with higher priority are offered excess bandwidth first. But rules about guaranteed rate (can't go below this value ) and ceil (maximum bandwith usable by a class) are still met.
+Classes with higher priority are offered excess bandwidth first. But rules about guaranteed rate (can't go below this value ) and ceil (maximum bandwidth usable by a class) are still met.
+
+This script automatically enable forwarding and source NAT
 
 To learn more about HTB take a look to the below links (thanks to the Author)
 
@@ -29,23 +60,73 @@ http://luxik.cdi.cz/~devik/qos/htb/manual/userg.htm
 
 http://luxik.cdi.cz/~devik/qos/htb/manual/theory.htm
 
-# Confguration and usage
+# Configuration and usage
 
-Edit the qos.cfg file and set the variables accordingly.
-For each variable there is a short explanation
+Edit the qos.cfg file and set the variables accordingly.For each variable there is a short explanation
 
-This script has been designed to use three different kind classes:
+- to disable the slowdown feature set the `ENABLE_SLOWDOWN` value to `off`
+- to disable the L7 classification set the `ENABLE_L7` value to `off`
 
--Bulk traffic
+To check supported applications run `iptables -m ndpi --help`
 
--Low priority traffic
+Usually the `iptables mark` parameters does not need to be changed,do it only if you know what are you doing
 
--High priority traffic
+**NO FILTER POLICY HAVE BEEN ADDED**
 
-There are also some rules that will slow down the connection if a big download is running (value SLOWDOWN_QUOTA in qos.cfg file).
-This script automatically enable forwarding and SOURCE NAT.
+The qos_class_mapping.cfg file contains a mapping (human readable) between the class ID and the class Description.
+If you will change the `iptables mark` values in the qos.cfg remember to update the qos_class_mapping.cfg accordingly.
 
-*NO FILTER POLICY HAVE BEEN ADDED*
+To run the script issue the below command
 
-The qos_class_mapping.cfg file contains a mapping between the class ID and the class Description.
-If you will change the classes marks values in the qos.cfg remember to update the qos_class_mapping.cfg accordingly.
+`./QoS.sh start`
+
+If you want to have some statistics about traffic QoS classes run
+
+`./QoS.sh stats`
+
+Below an example of the output
+
+```
+## Download QoS classes ##
++---(1:1) htb rate 1Gbit ceil 1Gbit burst 15125b cburst 1375b
+     |    Sent 1582 bytes 10 pkt (dropped 0, overlimits 0 requeues 0)
+     |    rate 0bit 0pps backlog 0b 0p requeues 0
+     |
+     +---(High-Priority-Download#1:11) htb prio 0 rate 1Mbit ceil 4Mbit burst 15Kb cburst 1600b
+     |                                 Sent 0 bytes 0 pkt (dropped 0, overlimits 0 requeues 0)
+     |                                 rate 0bit 0pps backlog 0b 0p requeues 0
+     |     
+     +---(Low-Priority-Download#1:10) htb prio 7 rate 512Kbit ceil 512Kbit burst 15Kb cburst 1600b
+     |                                Sent 0 bytes 0 pkt (dropped 0, overlimits 0 requeues 0)
+     |                                rate 0bit 0pps backlog 0b 0p requeues 0
+     |     
+     +---(Bulk-Download-Traffic#1:12) htb prio 5 rate 256Kbit ceil 1Mbit burst 15Kb cburst 1600b
+                                      Sent 1582 bytes 10 pkt (dropped 0, overlimits 0 requeues 0)
+                                      rate 0bit 0pps backlog 0b 0p requeues 0
+
+
+
+## Upload QoS classes ##
++---(1:1) htb rate 1Gbit ceil 1Gbit burst 15125b cburst 1375b
+     |    Sent 1249 bytes 16 pkt (dropped 0, overlimits 0 requeues 0)
+     |    rate 0bit 0pps backlog 0b 0p requeues 0
+     |
+     +---(Bulk-Upload-traffic#1:22) htb prio 5 rate 64Kbit ceil 2Mbit burst 15Kb cburst 1600b
+     |                              Sent 1249 bytes 16 pkt (dropped 0, overlimits 0 requeues 0)
+     |                              rate 0bit 0pps backlog 0b 0p requeues 0
+     |     
+     +---(Low-Priority-Upload#1:20) htb prio 7 rate 512Kbit ceil 1Mbit burst 15Kb cburst 1600b
+     |                              Sent 0 bytes 0 pkt (dropped 0, overlimits 0 requeues 0)
+     |                              rate 0bit 0pps backlog 0b 0p requeues 0
+     |     
+     +---(High-Priority-Upload#1:21) htb prio 0 rate 512Kbit ceil 2Mbit burst 15Kb cburst 1600b
+                                     Sent 0 bytes 0 pkt (dropped 0, overlimits 0 requeues 0)
+                                     rate 0bit 0pps backlog 0b 0p requeues 0
+```
+
+I decided to classify as bulk traffic all low ports traffic (0-1023) explicitly (except some tcp/udp ports.See Introduction).
+Actually,you can also classify as bulk all the not-classified traffic (i.e no high/low prio traffic) uncommenting
+the `#default $DOWN_BULK_MARK` line in QoS.sh file.
+
+# NOTES
+if you uncomment the `#default $DOWN_BULK_MARK` in QoS.sh,the locally-generated-traffic will be classified as bulk by default ,since this script classify only traffic that will be routed to WAN interface.
