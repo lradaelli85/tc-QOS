b744cc27d99793fae247c950c26694d31bee39e1
luca <l.radaelli85@noreply.github.com>
2019-04-05 14:50:09 +0200

    add new cfg parameters and NTP on high prio class

--- QoS.sh
+++ QoS.sh
@@ -3,6 +3,8 @@
 #load configurations
 . qos.cfg
 
+IPTMAN="iptables -t mangle"
+
 function add_system_configurations(){
 #https://bugzilla.redhat.com/show_bug.cgi?id=1011281
 #echo 1 >/sys/module/sch_htb/parameters/htb_rate_est
@@ -37,68 +39,68 @@ rmmod act_mirred
 
 
 function l7_classification(){
-iptables -t mangle -N QOS_DPI
+$IPTMAN -N QOS_DPI
 
 #Send everything to nDPI to applications detection
-iptables -t mangle -A PREROUTING -j QOS_DPI
+$IPTMAN -A PREROUTING -j QOS_DPI
 
 #DPI for applications
-iptables -t mangle -A QOS_DPI -m connmark --mark $APP_LOW_PRIO_MARK -j RETURN
-iptables -t mangle -A QOS_DPI -m connmark --mark $APP_BULK_MARK -j RETURN
-iptables -t mangle -A QOS_DPI -m connmark --mark $APP_HIGH_PRIO_MARK -j RETURN
-iptables -t mangle -A QOS_DPI -m connbytes --connbytes-mode packets --connbytes-dir both --connbytes 20 -j RETURN
+$IPTMAN -A QOS_DPI -m connmark --mark $APP_LOW_PRIO_MARK -j RETURN
+$IPTMAN -A QOS_DPI -m connmark --mark $APP_BULK_MARK -j RETURN
+$IPTMAN -A QOS_DPI -m connmark --mark $APP_HIGH_PRIO_MARK -j RETURN
+$IPTMAN -A QOS_DPI -m connbytes --connbytes-mode packets --connbytes-dir both --connbytes 20 -j RETURN
 if [ ${#LOW_PRIO_APP[@]} -gt 0 ]
   then
     for app in ${LOW_PRIO_APP[@]}
     do
-      iptables -t mangle -A QOS_DPI -m ndpi --$app -j CONNMARK --set-mark $APP_LOW_PRIO_MARK
+      $IPTMAN -A QOS_DPI -m ndpi --$app -j CONNMARK --set-mark $APP_LOW_PRIO_MARK
     done
 fi
 if [ ${#BULK_PRIO_APP[@]} -gt 0 ]
   then
     for app in ${BULK_PRIO_APP[@]}
     do
-      iptables -t mangle -A QOS_DPI -m ndpi --$app -j CONNMARK --set-mark $APP_BULK_MARK
+      $IPTMAN -A QOS_DPI -m ndpi --$app -j CONNMARK --set-mark $APP_BULK_MARK
     done
 fi
 if [ ${#HIGH_PRIO_APP[@]} -gt 0 ]
   then
     for app in ${HIGH_PRIO_APP[@]}
     do
-      iptables -t mangle -A QOS_DPI -m ndpi --$app -j CONNMARK --set-mark $APP_HIGH_PRIO_MARK
+      $IPTMAN -A QOS_DPI -m ndpi --$app -j CONNMARK --set-mark $APP_HIGH_PRIO_MARK
     done
 fi
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $APP_HIGH_PRIO_MARK -j CLASSIFY --set-class 1:$UP_HIGH_PRIO_MARK
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $APP_HIGH_PRIO_MARK -j RETURN
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $APP_LOW_PRIO_MARK -j CLASSIFY --set-class 1:$UP_LOW_PRIO_MARK
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $APP_LOW_PRIO_MARK -j RETURN
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $APP_BULK_MARK -j CLASSIFY --set-class 1:$UP_BULK_MARK
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $APP_BULK_MARK -j RETURN
+$IPTMAN -A QOS_UPLOAD -m mark --mark $APP_HIGH_PRIO_MARK -j CLASSIFY --set-class 1:$UP_HIGH_PRIO_MARK
+$IPTMAN -A QOS_UPLOAD -m mark --mark $APP_HIGH_PRIO_MARK -j RETURN
+$IPTMAN -A QOS_UPLOAD -m mark --mark $APP_LOW_PRIO_MARK -j CLASSIFY --set-class 1:$UP_LOW_PRIO_MARK
+$IPTMAN -A QOS_UPLOAD -m mark --mark $APP_LOW_PRIO_MARK -j RETURN
+$IPTMAN -A QOS_UPLOAD -m mark --mark $APP_BULK_MARK -j CLASSIFY --set-class 1:$UP_BULK_MARK
+$IPTMAN -A QOS_UPLOAD -m mark --mark $APP_BULK_MARK -j RETURN
 
 }
 
 function slowdown(){
-iptables -t mangle -N QOS_SLOWDOWN
-iptables -t mangle -I FORWARD -j QOS_SLOWDOWN
+$IPTMAN -N QOS_SLOWDOWN
+$IPTMAN -I FORWARD -j QOS_SLOWDOWN
 
 if [ ! -z ${TCP_SLOWDOWN_PORTS} ]
 then
-iptables -t mangle -A QOS_SLOWDOWN \
+$IPTMAN -A QOS_SLOWDOWN \
 -p tcp -m multiport --dports $TCP_SLOWDOWN_PORTS -m connbytes --connbytes $SLOWDOWN_QUOTA: \
 --connbytes-dir both --connbytes-mode bytes -j CONNMARK --set-mark $DOWN_LOW_PRIO_MARK
 
-iptables -t mangle -A QOS_SLOWDOWN \
+$IPTMAN -A QOS_SLOWDOWN \
 -p tcp -m multiport --dports $TCP_SLOWDOWN_PORTS -m connbytes --connbytes $SLOWDOWN_QUOTA: \
 --connbytes-dir both --connbytes-mode bytes -j RETURN
 fi
 
 if [ ! -z ${UDP_SLOWDOWN_PORTS} ]
 then
-iptables -t mangle -A QOS_SLOWDOWN \
+$IPTMAN -A QOS_SLOWDOWN \
 -p udp -m multiport --dports $UDP_SLOWDOWN_PORTS -m connbytes --connbytes $SLOWDOWN_QUOTA: \
 --connbytes-dir both --connbytes-mode bytes -j CONNMARK --set-mark $DOWN_LOW_PRIO_MARK
 
-iptables -t mangle -A QOS_SLOWDOWN \
+$IPTMAN -A QOS_SLOWDOWN \
 -p udp -m multiport --dports $UDP_SLOWDOWN_PORTS -m connbytes --connbytes $SLOWDOWN_QUOTA: \
 --connbytes-dir both --connbytes-mode bytes -j RETURN
 fi
@@ -111,17 +113,17 @@ if [ $ENABLE_NAT == "on" ]
     iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
 fi
 #Qos chains
-iptables -t mangle -N QOS_UPLOAD
-iptables -t mangle -N QOS_DOWNLOAD
-iptables -t mangle -N RESTORE-MARK
-iptables -t mangle -N SAVE-MARK
-iptables -t mangle -A FORWARD -m mark --mark 0 -o $WAN -j QOS_DOWNLOAD
-iptables -t mangle -A FORWARD -o $WAN -m mark ! --mark 0 -j QOS_UPLOAD
-iptables -t mangle -A PREROUTING -m connmark ! --mark 0 -j RESTORE-MARK
-iptables -t mangle -A POSTROUTING -m mark ! --mark 0 -j SAVE-MARK
+$IPTMAN -N QOS_UPLOAD
+$IPTMAN -N QOS_DOWNLOAD
+$IPTMAN -N RESTORE-MARK
+$IPTMAN -N SAVE-MARK
+$IPTMAN -A FORWARD -m mark --mark 0 -o $WAN -j QOS_DOWNLOAD
+$IPTMAN -A FORWARD -o $WAN -m mark ! --mark 0 -j QOS_UPLOAD
+$IPTMAN -A PREROUTING -m connmark ! --mark 0 -j RESTORE-MARK
+$IPTMAN -A POSTROUTING -m mark ! --mark 0 -j SAVE-MARK
 
 #restore mark for previously marked connection
-iptables -t mangle -A RESTORE-MARK -m conntrack ! --ctstate NEW -j CONNMARK --restore-mark
+$IPTMAN -A RESTORE-MARK -m conntrack ! --ctstate NEW -j CONNMARK --restore-mark
 
 if [ $ENABLE_L7 == "on" ]
   then
@@ -135,57 +137,56 @@ fi
 
 #high prio traffic
 #http(s),ssh
-iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_HIGH_PRIO_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_HIGH_PRIO_PORTS \
 -m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_HIGH_PRIO_MARK
 
-iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_HIGH_PRIO_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_HIGH_PRIO_PORTS \
 -m conntrack --ctstate NEW -j RETURN
 
-#voip,dns,ipsec,openvpn
-iptables -t mangle -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_HIGH_PRIO_PORTS \
+#voip,dns,ipsec,openvpn,NTP
+$IPTMAN -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_HIGH_PRIO_PORTS \
 -m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_HIGH_PRIO_MARK
 
-iptables -t mangle -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_HIGH_PRIO_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_HIGH_PRIO_PORTS \
 -m conntrack --ctstate NEW -j RETURN
 
 #bulk traffic
-iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_BULK_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_BULK_PORTS \
 -m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_BULK_MARK
 
-iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_BULK_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_BULK_PORTS \
 -m conntrack --ctstate NEW -j RETURN
 
-iptables -t mangle -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_BULK_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_BULK_PORTS \
 -m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_BULK_MARK
 
-iptables -t mangle -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_BULK_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_BULK_PORTS \
 -m conntrack --ctstate NEW -j RETURN
 
 #low prio traffic
-iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_LOW_PRIO_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_LOW_PRIO_PORTS \
 -m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_LOW_PRIO_MARK
 
-iptables -t mangle -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_LOW_PRIO_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p tcp -m multiport --dports $TCP_LOW_PRIO_PORTS \
 -m conntrack --ctstate NEW -j RETURN
 
-iptables -t mangle -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_LOW_PRIO_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_LOW_PRIO_PORTS \
 -m conntrack --ctstate NEW -j CONNMARK --set-mark $DOWN_LOW_PRIO_MARK
 
-iptables -t mangle -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_LOW_PRIO_PORTS \
+$IPTMAN -A QOS_DOWNLOAD -p udp -m multiport --dports $UDP_LOW_PRIO_PORTS \
 -m conntrack --ctstate NEW -j RETURN
 
 
 #In this way i should not need to mark connection for upload,and the mark for download traffic shuold be maintained
-#high prio traffic
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $DOWN_HIGH_PRIO_MARK -j CLASSIFY --set-class 1:$UP_HIGH_PRIO_MARK
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $DOWN_HIGH_PRIO_MARK -j RETURN
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $DOWN_LOW_PRIO_MARK -j CLASSIFY --set-class 1:$UP_LOW_PRIO_MARK
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $DOWN_LOW_PRIO_MARK -j RETURN
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $DOWN_BULK_MARK -j CLASSIFY --set-class 1:$UP_BULK_MARK
-iptables -t mangle -A QOS_UPLOAD -m mark --mark $DOWN_BULK_MARK -j RETURN
+$IPTMAN -A QOS_UPLOAD -m mark --mark $DOWN_HIGH_PRIO_MARK -j CLASSIFY --set-class 1:$UP_HIGH_PRIO_MARK
+$IPTMAN -A QOS_UPLOAD -m mark --mark $DOWN_HIGH_PRIO_MARK -j RETURN
+$IPTMAN -A QOS_UPLOAD -m mark --mark $DOWN_LOW_PRIO_MARK -j CLASSIFY --set-class 1:$UP_LOW_PRIO_MARK
+$IPTMAN -A QOS_UPLOAD -m mark --mark $DOWN_LOW_PRIO_MARK -j RETURN
+$IPTMAN -A QOS_UPLOAD -m mark --mark $DOWN_BULK_MARK -j CLASSIFY --set-class 1:$UP_BULK_MARK
+$IPTMAN -A QOS_UPLOAD -m mark --mark $DOWN_BULK_MARK -j RETURN
 
 # #save mark of the previously marked connections
-iptables -t mangle -A SAVE-MARK -m conntrack --ctstate NEW -j CONNMARK --save-mark
+$IPTMAN -A SAVE-MARK -m conntrack --ctstate NEW -j CONNMARK --save-mark
 }
 
 function add_qos_devs_and_classes(){
@@ -199,19 +200,19 @@ if [ $BULK_DEFAULT == "on" ]
 fi
 
 #set global download value
-tc class add dev $IFB parent 1: classid 1:1 htb rate $WAN_DOWNLOAD burst 15k
+tc class add dev $IFB parent 1: classid 1:1 htb rate $WAN_DOWNLOAD burst $BURST
 
 #high priority class
 tc class add dev $IFB parent 1:1 classid 1:$DOWN_HIGH_PRIO_MARK htb rate $HIGH_PRIO_DOWN_GUARANTEED  \
-ceil $HIGH_PRIO_DOWN_MAX quantum 1514 burst 15k prio 0
+ceil $HIGH_PRIO_DOWN_MAX quantum $QUANTUM burst $BURST prio 0
 
 #low priority class
 tc class add dev $IFB parent 1:1 classid 1:$DOWN_LOW_PRIO_MARK htb rate $LOW_PRIO_DOWN_GUARANTEED \
-ceil $LOW_PRIO_DOWN_MAX quantum 1514 burst 15k prio 9
+ceil $LOW_PRIO_DOWN_MAX quantum $QUANTUM burst $BURST prio 7
 
 #bulk traffic class
 tc class add dev $IFB parent 1:1 classid 1:$DOWN_BULK_MARK htb rate $DOWNLOAD_GUARANTEED_DEFAULT \
-ceil $DOWNLOAD_MAX_DEFAULT quantum 1514 burst 15k prio 5
+ceil $DOWNLOAD_MAX_DEFAULT quantum $QUANTUM burst $BURST prio 5
 
 #use class [high prio]
 tc filter add dev $IFB parent 1:0 protocol ip handle $DOWN_HIGH_PRIO_MARK fw flowid 1:$DOWN_HIGH_PRIO_MARK
@@ -253,16 +254,19 @@ if [ $BULK_DEFAULT == "on" ]
 fi
 
 #set the global upload value
-tc class add dev $WAN parent 1: classid 1:1 htb rate $WAN_UPLOAD burst 15k
+tc class add dev $WAN parent 1: classid 1:1 htb rate $WAN_UPLOAD burst $BURST
 
 #high priority class
-tc class add dev $WAN parent 1:1 classid 1:$UP_HIGH_PRIO_MARK htb rate $HIGH_PRIO_UP_GUARANTEED ceil $HIGH_PRIO_UP_MAX quantum 1514 burst 15k prio 0
+tc class add dev $WAN parent 1:1 classid 1:$UP_HIGH_PRIO_MARK htb rate $HIGH_PRIO_UP_GUARANTEED \
+ceil $HIGH_PRIO_UP_MAX quantum $QUANTUM burst $BURST prio 0
 
 #low priority class
-tc class add dev $WAN parent 1:1 classid 1:$UP_LOW_PRIO_MARK htb rate $LOW_PRIO_UP_GUARANTEED ceil $LOW_PRIO_UP_MAX quantum 1514 burst 15k prio 9
+tc class add dev $WAN parent 1:1 classid 1:$UP_LOW_PRIO_MARK htb rate $LOW_PRIO_UP_GUARANTEED \
+ceil $LOW_PRIO_UP_MAX quantum $QUANTUM burst $BURST prio 7
 
 #bulk traffic class
-tc class add dev $WAN parent 1:1 classid 1:$UP_BULK_MARK htb rate $UPLOAD_GUARANTEED_DEFAULT ceil $UPLOAD_MAX_DEFAULT quantum 1514 burst 15k prio 5
+tc class add dev $WAN parent 1:1 classid 1:$UP_BULK_MARK htb rate $UPLOAD_GUARANTEED_DEFAULT \
+ceil $UPLOAD_MAX_DEFAULT quantum $QUANTUM burst $BURST prio 5
 
 #Tell which algorithm the classes use
 tc qdisc add dev $WAN parent 1:$UP_HIGH_PRIO_MARK sfq perturb 10
@@ -279,29 +283,29 @@ tc qdisc del dev $IFB root
 
 function del_iptables_rules(){
 #flush iptables rules
-iptables -t mangle -F SAVE-MARK
-iptables -t mangle -D POSTROUTING -m mark ! --mark 0 -j SAVE-MARK
-iptables -t mangle -X SAVE-MARK
-iptables -t mangle -F RESTORE-MARK
-iptables -t mangle -D PREROUTING -m connmark ! --mark 0 -j RESTORE-MARK
-iptables -t mangle -X RESTORE-MARK
+$IPTMAN -F SAVE-MARK
+$IPTMAN -D POSTROUTING -m mark ! --mark 0 -j SAVE-MARK
+$IPTMAN -X SAVE-MARK
+$IPTMAN -F RESTORE-MARK
+$IPTMAN -D PREROUTING -m connmark ! --mark 0 -j RESTORE-MARK
+$IPTMAN -X RESTORE-MARK
 if [ $ENABLE_SLOWDOWN == "on" ]
   then
-    iptables -t mangle -F QOS_SLOWDOWN
-    iptables -t mangle -D FORWARD -j QOS_SLOWDOWN
-    iptables -t mangle -X QOS_SLOWDOWN
+    $IPTMAN -F QOS_SLOWDOWN
+    $IPTMAN -D FORWARD -j QOS_SLOWDOWN
+    $IPTMAN -X QOS_SLOWDOWN
 fi
-iptables -t mangle -F QOS_DOWNLOAD
-iptables -t mangle -D FORWARD -m mark --mark 0 -o $WAN -j QOS_DOWNLOAD
-iptables -t mangle -X QOS_DOWNLOAD
-iptables -t mangle -F QOS_UPLOAD
-iptables -t mangle -D FORWARD -o $WAN -m mark ! --mark 0 -j QOS_UPLOAD
-iptables -t mangle -X QOS_UPLOAD
+$IPTMAN -F QOS_DOWNLOAD
+$IPTMAN -D FORWARD -m mark --mark 0 -o $WAN -j QOS_DOWNLOAD
+$IPTMAN -X QOS_DOWNLOAD
+$IPTMAN -F QOS_UPLOAD
+$IPTMAN -D FORWARD -o $WAN -m mark ! --mark 0 -j QOS_UPLOAD
+$IPTMAN -X QOS_UPLOAD
 if [ $ENABLE_L7 == "on" ]
   then
-    iptables -t mangle -F QOS_DPI
-    iptables -t mangle -D PREROUTING -j QOS_DPI
-    iptables -t mangle -X QOS_DPI
+    $IPTMAN -F QOS_DPI
+    $IPTMAN -D PREROUTING -j QOS_DPI
+    $IPTMAN -X QOS_DPI
 fi
 if [ $ENABLE_NAT == "on" ]
   then
